c456e9de371f06a434f7fd3ca5694796daf8deac
Shimpei Makimoto <makimoto@tsuyabu.in>
2012-03-30 00:39:02 +0900

    Add test cases and impl romaji to kana method

--- spec/romaji_spec.rb
+++ spec/romaji_spec.rb
@@ -1,9 +1,49 @@
 # coding: utf-8
 
 require 'spec_helper'
+require 'romaji'
 
 describe Romaji do
-  it 'should pass' do
-    42.should == 42
+  context 'transliterate from romaji to kana' do
+    it 'should transliterate' do
+      Romaji.romaji2kana('kyoumoshinaitone').should == 'キョウモシナイトネ'
+      Romaji.romaji2kana('SushiNoTabetas').should == 'スシノタベタs'
+    end
+
+    it 'should transliterate with kana_type' do
+      Romaji.romaji2kana('kyoumoshinaitone', :kana_type => :hiragana).should == 'きょうもしないとね'
+    end
+  end
+
+  it 'should transliterate from kana to romaji' do
+    Romaji.kana2romaji('キョウモシナイトネ').should == 'kyoumoshinaitone'
+
+    pending 'not impl\'ed'
+    Romaji.kana2romaji('すしのたべたさ').should == 'sushinotabetasa'
+  end
+  
+  describe Romaji::StringExtension do
+    before  do
+      @sushi = 'ｽｼ'
+      @abc = 'ａＢC'
+      @sushi.extend(Romaji::StringExtension)
+      @abc.extend(Romaji::StringExtension)
+    end
+
+    it 'should extend String#normalize' do
+      @sushi.normalize.should == 'スシ'
+      @sushi.should == 'ｽｼ'
+
+      @abc.normalize.should == 'abc'
+      @abc.should == 'ａＢC'
+    end
+
+    it 'should extend String#normalize!' do
+      @sushi.normalize!
+      @sushi.should == 'スシ'
+
+      @abc.normalize!
+      @abc.should == 'abc'
+    end
   end
 end
